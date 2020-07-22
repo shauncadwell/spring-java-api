a3c95f6007eb0ed21fce1f2062eecc21da5566ff
Shaun <loglution@gmail.com>
2020-07-22 04:45:11 +0000

    removed subtree

--- src/main/java/com/frostylog/spring/api/ApiApplication.java
+++ src/main/java/com/frostylog/spring/api/ApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		// Person person = new Person();
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
