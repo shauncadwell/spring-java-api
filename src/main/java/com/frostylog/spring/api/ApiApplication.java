f18e85ecb7e291be5e3c689837447978e26a9027
Shaun <loglution@gmail.com>
2020-07-24 03:09:54 +0000

    Added call to UPS api - removed licenseNumber

--- src/main/java/com/frostylog/spring/api/ApiApplication.java
+++ src/main/java/com/frostylog/spring/api/ApiApplication.java
@@ -1,7 +1,18 @@
 package com.frostylog.spring.api;
 
-import com.frostylog.spring.lib.models.Hair;
-import com.frostylog.spring.lib.models.Person;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.file.Paths;
+import java.time.Duration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,12 +21,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		// Person person = new Person();
-		// person.setAddress("Address");
-		// Hair hair = new Hair("brown", "short");
-		// person.setHair(hair);
-		// System.out.println(person);
+
 		SpringApplication.run(ApiApplication.class, args);
+		doRequest();
+	}
+
+	public static void doRequest() {
+		String code = "1Z81RR270372827930";
+		String strUrl = "https://onlinetools.ups.com/track/v1/details/" + code + "?locale=en_US";
+		String accessLicenseNumber = "";
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(strUrl))
+				.setHeader("Content-Type", "application/json").setHeader("Accept", "*/*")
+				.setHeader("AccessLicenseNumber", accessLicenseNumber).build();
+		client.sendAsync(request, BodyHandlers.ofString()).thenApply(HttpResponse::body).thenAccept(System.out::println)
+				.join();
+
 	}
 
 }
--- src/main/java/com/frostylog/spring/api/controllers/GenericController.java
+++ src/main/java/com/frostylog/spring/api/controllers/GenericController.java
@@ -2,7 +2,6 @@ package com.frostylog.spring.api.controllers;
 
 import com.frostylog.spring.lib.models.Person;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GenericController {
+
     @RequestMapping(value = "/api/v1/", method = RequestMethod.POST)
     public Person getPersonByName(@RequestBody final Person pPerson) {
         // Will echo object that is passed to it.
